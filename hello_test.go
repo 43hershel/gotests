47cac7a59c6aabe09e2cca0b2cc566cd4917c31e
castro <xcaze8@gmail.com>
2024-03-04 08:47:27 +0100

    Refactor testing parameters

--- hello_test.go
+++ hello_test.go
@@ -5,9 +5,19 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris!"
-
+	t.Run("saying Hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello World!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
