751315a088f0776b044b207473ebddab2283b98f
castro <xcaze8@gmail.com>
2024-03-08 14:44:09 +0100

    Added math tester and repeat test

---
+++ integers/adder_test.go
@@ -0,0 +1,25 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(5, 2)
+	fmt.Println(sum)
+}
+
+// Add takes two integers and returns the sum of them.
+func Add(x, y int) int {
+	return x + y
+}
